06d51159f5f920afdbb4beadf70773cf2fe9b79b
federicocelico <federico.celico@gmail.com>
2021-01-18 12:29:13 -0300

    First Commit

---
+++ APIRest/Models/BaseDeDatos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace APIRest.Models
+{
+    public class BaseDeDatos : DbContext 
+    {
+        public BaseDeDatos()
+            :base("Conexion")
+        {
+
+        }
+        public DbSet<Producto> Productos { get; set; }
+
+    }
+}
---
+++ APIRest/Models/Producto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace APIRest.Models
+{
+    public class Producto
+    {
+        [Key]
+        public int idArticulo { get; set; }
+        public string Descripcion { get; set; }
+        public double Precio { get; set; }
+        public int Stock { get; set; }
+        public int Activo { get; set; }
+
+    }
+}
